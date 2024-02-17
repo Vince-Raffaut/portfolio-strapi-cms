280d469dedbddcc36bf56708515b7c355e48e7d3
Vince Raffaut <vince.raffaut@gmail.com>
2024-02-17 15:41:47 +1100

    updating cors for strapi]

--- config/middlewares.js
+++ config/middlewares.js
@@ -4,6 +4,14 @@ module.exports = [
   'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: ['http://localhost:1337', 'http://vince-raffaut.dev']
+    }
+  },
   'strapi::query',
   'strapi::body',
   'strapi::session',
